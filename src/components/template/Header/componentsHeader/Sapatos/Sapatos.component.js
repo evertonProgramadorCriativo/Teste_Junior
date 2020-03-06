8262bea791160bcf6c4f40ebb8025e833a5e583a
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2020-03-06 14:15:10 -0300

    verticalDots e mainBox  esta com as fotos

--- src/components/template/Header/componentsHeader/Sapatos/Sapatos.component.js
+++ src/components/template/Header/componentsHeader/Sapatos/Sapatos.component.js
@@ -1,28 +1,32 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import './Sapatos.styles.scss';
+import { ReactComponent as Sandaria } from '../../../../../assets/images/Produto02Sandaria.svg';
+import { ReactComponent as SandariaGG } from '../../../../../assets/images/SandariaGG.svg';
+
+
 
 function SampleNextArrow(props) {
     const { className, style, onClick } = props;
     return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "black", left: "411px", top: "-41px" , position: "relative"  }}
-        onClick={onClick}
-      />
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "black", left: "411px", top: "-41px", position: "relative" }}
+            onClick={onClick}
+        />
     );
-  }
-  
-  function SamplePrevArrow(props) {
+}
+
+function SamplePrevArrow(props) {
     const { className, style, onClick } = props;
     return (
-      <div
-        className={className}
-        style={{ ...style, display: "block", background: "black", left: "-32px", top: "72px" , position: "relative"   }}
-        onClick={onClick}
-      />
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "black", left: "-32px", top: "72px", position: "relative" }}
+            onClick={onClick}
+        />
     );
-  }
+}
 
 
 
@@ -50,16 +54,16 @@ export default class AsNavFor extends Component {
             slidesToScroll: 1,
             nextArrow: <SampleNextArrow />,
             prevArrow: <SamplePrevArrow />
-          };
+        };
         return (
             <React.Fragment>
-            <nav className="sapatos">
-                <div className="sapatos_rotaAtual">
-                    <span className="sapatos_rotaAtual--nome">Home</span>
-                    <span className="sapatos_rotaAtual--separador">/ </span>
-                    <span className="sapatos_rotaAtual--nome">Sapatos</span>
-                </div>
-                <div className="sapatos_Slider">
+                <nav className="sapatos">
+                    <div className="sapatos_rotaAtual">
+                        <span className="sapatos_rotaAtual--nome">Home</span>
+                        <span className="sapatos_rotaAtual--separador">/ </span>
+                        <span className="sapatos_rotaAtual--nome">Sapatos</span>
+                    </div>
+                    <div className="sapatos_Slider">
 
                         <Slider className="sapatos_Slider--video"
                             asNavFor={this.state.nav1}
@@ -68,7 +72,7 @@ export default class AsNavFor extends Component {
                             swipeToSlide={true}
                             focusOnSelect={true}
                             arrows={false}
-                            
+
 
                         >
 
@@ -90,110 +94,115 @@ export default class AsNavFor extends Component {
                             ref={slider => (this.slider2 = slider)}
                             slidesToShow={4}
                             {...settings}
-                           
+
                         >
 
 
                             <div className="sapatos_Slider--VerticalDots-box">
-                                <h3 className="sapatos_Slider--VerticalDots-img">1</h3>
+                                <Sandaria className="sapatos_Slider--VerticalDots-img" />
                             </div>
                             <div className="sapatos_Slider--VerticalDots-box">
-                                <h3 className="sapatos_Slider--VerticalDots-img">2</h3>
+                                <Sandaria className="sapatos_Slider--VerticalDots-img" />
                             </div>
                             <div className="sapatos_Slider--VerticalDots-box">
-                                <h3 className="sapatos_Slider--VerticalDots-img">3</h3>
+                                <Sandaria className="sapatos_Slider--VerticalDots-img" />
                             </div>
                             <div className="sapatos_Slider--VerticalDots-box">
-                                <h3 className="sapatos_Slider--VerticalDots-img">4</h3>
+                                <Sandaria className="sapatos_Slider--VerticalDots-img" />
                             </div>
                             <div className="sapatos_Slider--VerticalDots-box">
-                                <h3 className="sapatos_Slider--VerticalDots-img">5</h3>
+                                <Sandaria className="sapatos_Slider--VerticalDots-img" />
                             </div>
                             <div className="sapatos_Slider--VerticalDots-box">
-                                <h3 className="sapatos_Slider--VerticalDots-img">6</h3>
+                                <Sandaria className="sapatos_Slider--VerticalDots-img" />
                             </div>
 
                         </Slider>
 
-                  
-                    
-
-
-                    <Slider className="sapatos_Slider--MainBox"
-                        asNavFor={this.state.nav2}
-                        ref={slider => (this.slider1 = slider)}
-                    >
-                        <div className="sapatos_Slider--MainBox-box">
-                            <h3 className="sapatos_Slider--MainBox-img">1</h3>
-
-                        </div>
-                        <div className="sapatos_Slider--MainBox-box">
-                            <h3 className="sapatos_Slider--MainBox-img">2</h3>
-                        </div>
-                        <div className="sapatos_Slider--MainBox-box">
-                            <h3 className="sapatos_Slider--MainBox-img">3</h3>
-                        </div>
-                        <div className="sapatos_Slider--MainBox-box">
-                            <h3 className="sapatos_Slider--MainBox-img">4</h3>
-                        </div>
-                        <div className="sapatos_Slider--MainBox-box">
-                            <h3 className="sapatos_Slider--MainBox-img">5</h3>
-                        </div>
-                        <div className="sapatos_Slider--MainBox-box">
-                            <h3 className="sapatos_Slider--MainBox-img">6</h3>
-                        </div>
-                    </Slider>
 
 
 
-                    <Slider className="sapatos_Slider--BoxDetalhes"
-                        asNavFor={this.state.nav1}
-                        ref={slider => (this.slider2 = slider)}
-                        slidesToShow={1}
-                        swipeToSlide={true}
-                        focusOnSelect={true}
-                        arrows={false}
 
-                    >
+                        <Slider className="sapatos_Slider--MainBox"
+                            asNavFor={this.state.nav2}
+                            ref={slider => (this.slider1 = slider)}
+                        >
+                            <div className="sapatos_Slider--MainBox-box">
+                                <SandariaGG className="sapatos_Slider--MainBox-img" />
 
+                            </div>
+                            <div className="sapatos_Slider--MainBox-box">
+                                <SandariaGG className="sapatos_Slider--MainBox-img" />
 
-                        <div className="sapatos_Slider--BoxDetalhes-box">
-                            <h3 className="sapatos_Slider--BoxDetalhes-img">1</h3>
+                            </div>
+                            <div className="sapatos_Slider--MainBox-box">
+                                <SandariaGG className="sapatos_Slider--MainBox-img" />
 
-                        </div>
+                            </div>
+                            <div className="sapatos_Slider--MainBox-box">
+                                <SandariaGG className="sapatos_Slider--MainBox-img" />
 
-                        <div className="sapatos_Slider--BoxDetalhes-box">
-                            <h3 className="sapatos_Slider--BoxDetalhes-img">2</h3>
+                            </div>
+                            <div className="sapatos_Slider--MainBox-box">
+                                <SandariaGG className="sapatos_Slider--MainBox-img" />
 
-                        </div>
+                            </div>
+                            <div className="sapatos_Slider--MainBox-box">
+                                <SandariaGG className="sapatos_Slider--MainBox-img" />
 
-                        <div className="sapatos_Slider--BoxDetalhes-box">
-                            <h3 className="sapatos_Slider--BoxDetalhes-img">3</h3>
-
-                        </div>
-
-                        <div className="sapatos_Slider--BoxDetalhes-box">
-                            <h3 className="sapatos_Slider--BoxDetalhes-img">4</h3>
-
-                        </div>
-
-
-
-                        <div className="sapatos_Slider--BoxDetalhes-box">
-                            <h3 className="sapatos_Slider--BoxDetalhes-img">5</h3>
-
-                        </div>
-
-                        <div className="sapatos_Slider--BoxDetalhes-box">
-                            <h3 className="sapatos_Slider--BoxDetalhes-img">6</h3>
-
-                        </div>
-                    </Slider>
+                            </div>
+                        </Slider>
 
 
-                </div>
 
-            </nav>
+                        <Slider className="sapatos_Slider--BoxDetalhes"
+                            asNavFor={this.state.nav1}
+                            ref={slider => (this.slider2 = slider)}
+                            slidesToShow={1}
+                            swipeToSlide={true}
+                            focusOnSelect={true}
+                            arrows={false}
+
+                        >
+
+
+                            <div className="sapatos_Slider--BoxDetalhes-box">
+                                <h3 className="sapatos_Slider--BoxDetalhes-img">1</h3>
+
+                            </div>
+
+                            <div className="sapatos_Slider--BoxDetalhes-box">
+                                <h3 className="sapatos_Slider--BoxDetalhes-img">2</h3>
+
+                            </div>
+
+                            <div className="sapatos_Slider--BoxDetalhes-box">
+                                <h3 className="sapatos_Slider--BoxDetalhes-img">3</h3>
+
+                            </div>
+
+                            <div className="sapatos_Slider--BoxDetalhes-box">
+                                <h3 className="sapatos_Slider--BoxDetalhes-img">4</h3>
+
+                            </div>
+
+
+
+                            <div className="sapatos_Slider--BoxDetalhes-box">
+                                <h3 className="sapatos_Slider--BoxDetalhes-img">5</h3>
+
+                            </div>
+
+                            <div className="sapatos_Slider--BoxDetalhes-box">
+                                <h3 className="sapatos_Slider--BoxDetalhes-img">6</h3>
+
+                            </div>
+                        </Slider>
+
+
+                    </div>
+
+                </nav>
             </React.Fragment>
         );
     }
